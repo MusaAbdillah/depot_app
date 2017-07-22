987c0d438eec275f684516a2af6e23768469650a
Musa Abdillah <musaabdillahm9@gmail.com>
2017-07-22 09:27:48 +0700

    new depot

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,15 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-<<<<<<< HEAD
-  
-=======
-  config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.mailgun_settings = {
-    api_key: 'pubkey-2e88134554f0f2097c5ddcaf1f16fe98',
-    domain: 'sandbox4bfa1ee94f2d40bc85dd52ec35e4b518.mailgun.org'
-  }
->>>>>>> 477480aadf661d9de5a2d214c7a4e869660e3ab7
 
   
 end
