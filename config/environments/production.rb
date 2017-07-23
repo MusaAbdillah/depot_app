9f3e3d7b812a4bf8ff7a472540e7e6738e772c62
Musa Abdillah <musaabdillahm9@gmail.com>
2017-07-23 14:44:29 +0700

    add config on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,9 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { :host => 'polar-badlands-76474.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
     # gmail configuration
       config.action_mailer.smtp_settings = {
         address:              'smtp.gmail.com',
