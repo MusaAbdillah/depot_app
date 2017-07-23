2b7c38ee6edacfb1d3ae6cc2f192d10b5b3d267e
Musa Abdillah <musaabdillahm9@gmail.com>
2017-07-23 14:54:11 +0700

    change config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,20 +76,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'polar-badlands-76474.herokuapp.com' }
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  ActionMailer::Base.perform_deliveries = true
-  ActionMailer::Base.raise_delivery_errors = true
-    # gmail configuration
-      config.action_mailer.smtp_settings = {
-        address:              'smtp.gmail.com',
-        port:                 587,
-        domain:               'mail.google.com',
-        user_name:            ENV["GMAIL_USERNAME"],
-        password:             ENV["GMAIL_PASSWORD"],
-        authentication:       'login',
-        enable_starttls_auto: true
-      }
+  
+  config.action_mailer.default_url_options = { :host => 'polar-badlands-76474.herokuapp.com' }  
+    config.action_mailer.delivery_method = :smtp  
+    config.action_mailer.perform_deliveries = true  
+    config.action_mailer.raise_delivery_errors = false  
+    config.action_mailer.default :charset => "utf-8"  
+    config.action_mailer.smtp_settings = {  
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "polar-badlands-76474.herokuapp.com",
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: ENV["GMAIL_USERNAME"],
+      password: ENV["GMAIL_PASSWORD"]
+    }
   
 end
