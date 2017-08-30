fdaffb95aa26a450098542331bdf6165f8b6a22b
MusaAbdillah <musaabdillahm9@gmail.com>
2017-08-30 17:27:57 +0700

    change layout for better view with bootstrap

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'will_paginate',           '3.0.7'
 gem 'bootstrap-will_paginate', '0.0.10'
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'simple_form'
+gem "font-awesome-rails"
 
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
 	after_destroy :ensure_an_admin_remains
-  	validates :name, presence: true, uniqueness: true
-  	validates :phone, :email, :gender, presence: true
-  	has_secure_password
+  validates :name, presence: true, uniqueness: true
+  validates :phone, :email, :gender, presence: true
+  has_secure_password
 
   	private 
+      #method for ensure that admin still ready on databases
   		def ensure_an_admin_remains
   			if User.count.zero?
   				raise "Can't delete last user"
