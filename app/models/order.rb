f1704f634656ae1fb9c639515aa80c6e82cca584
MusaAbdillah <musaabdillahm9@gmail.com>
2017-09-13 21:37:29 +0700

    changin layout

--- app/models/order.rb
+++ app/models/order.rb
@@ -14,3 +14,53 @@ class Order < ActiveRecord::Base
 	end
 
 end
+
+
+
+# require 'uri'
+# require 'net/http'
+
+# url = URI("https://api.rajaongkir.com/starter/province")
+
+# http = Net::HTTP.new(url.host, url.port)
+# http.use_ssl = true
+# http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+# request = Net::HTTP::Get.new(url)
+# request["key"] = '2e2597d10cfb3b707a8c678632ddfdff'
+
+# response = http.request(request)
+# puts response.read_body
+
+# require 'uri'
+# require 'net/http'
+
+# url = URI("https://api.rajaongkir.com/starter/city?id=39&province=5")
+
+# http = Net::HTTP.new(url.host, url.port)
+# http.use_ssl = true
+# http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+# request = Net::HTTP::Get.new(url)
+# request["key"] = '2e2597d10cfb3b707a8c678632ddfdff'
+
+# response = http.request(request)
+# puts response.read_body
+
+# require 'uri'
+# require 'net/http'
+
+# url = URI("https://api.rajaongkir.com/starter/cost")
+
+# http = Net::HTTP.new(url.host, url.port)
+# http.use_ssl = true
+# http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+# request = Net::HTTP::Post.new(url)
+# request["key"] = '2e2597d10cfb3b707a8c678632ddfdff'
+# request["content-type"] = 'application/x-www-form-urlencoded'
+# request.body = "origin=501&destination=114&weight=1700&courier=jne"
+
+# response = http.request(request)
+# puts response.read_body
+
